be777ebff149b0c6ea33e4eff14da7c911dd487e
AKamci <abdulkadir.kamci10@gmail.com>
2024-03-26 16:07:08 +0300

    DB first with console app.

---
+++ EF-Core/EF-DatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_DatabaseFirst.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-ABDULKA; Database =EF- CoreDB-First; Trusted_Connection = True; Encrypt = true; TrustServerCertificate = True");
+
+
+
+        }
+
+    }
+}
+ 
---
+++ EF-Core/EF-DatabaseFirst/Program.cs
@@ -0,0 +1,13 @@
+﻿using EF_DatabaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+using (var _context =new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+	foreach (var item in products)
+	{
+		Console.WriteLine($"{item.Id} : {item.Name} : {item.Price}");
+	}
+
+}
